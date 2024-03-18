aea8e7c8059760c4426fbba91c5e5453a0909697
Yoo Jeonghwa <green2368@naver.com>
2024-03-18 15:52:09 +0900

    Modify toc.js (unfold all list)
    모든 header가 toc에 펼친 상태로 보이도록 설정. header1도 인식하도록 설정

--- _javascript/modules/components/toc.js
+++ _javascript/modules/components/toc.js
@@ -1,13 +1,14 @@
 export function toc() {
-  if (document.querySelector('main h2, main h3')) {
+  if (document.querySelector('main h1, main h2, main h3')) {
     // see: https://github.com/tscanlin/tocbot#usage
     tocbot.init({
       tocSelector: '#toc',
       contentSelector: '.content',
       ignoreSelector: '[data-toc-skip]',
-      headingSelector: 'h2, h3, h4',
+      headingSelector: 'h1, h2, h3, h4',
       orderedList: false,
-      scrollSmooth: false
+      scrollSmooth: false,
+      collapseDepth: 6,
     });
   }
 }
