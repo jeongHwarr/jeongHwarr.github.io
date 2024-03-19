1b829f25fc72679040458a6bb13a9ca1ca1f4175
Yoo Jeonghwa <green2368@naver.com>
2024-03-19 14:42:48 +0900

    Update toc.js

--- _javascript/modules/components/toc.js
+++ _javascript/modules/components/toc.js
@@ -8,7 +8,7 @@ export function toc() {
       headingSelector: 'h1, h2, h3, h4',
       orderedList: false,
       scrollSmooth: false,
-      collapseDepth: 6,
+      collapseDepth: 6, //unfold all list
     });
   }
 }
